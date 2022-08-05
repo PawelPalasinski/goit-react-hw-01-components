a24bc1665e0ea74d85a8a205d6bcbc5120081c82
PawelPalasinski <pawel.palasinski@gmail.com>
2022-08-06 01:20:31 +0200

    statistics

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,19 +1,30 @@
 import { Profile } from "./Profile/Profile";
 import user from "../data/user.json";
 
+import { Statistics } from "./Statistics/Statistics";
+import data from "../data/data.json"
+
 
 export const App = () => {
   const {username, tag, location, avatar, stats} = user;
-  
+
   return (
     <>
-      <Profile 
-        username={username}
-        tag={tag}
-        location={location}
-        avatar={avatar}
-        stats={stats}
-      />
-    </>
+      <>
+        <Profile 
+          username={username}
+          tag={tag}
+          location={location}
+          avatar={avatar}
+          stats={stats}
+        />
+      </>
+
+      <>
+        <Statistics title="Upload stats" stats={data} />
+      </>
+      
+      </>
+
   );
 };
