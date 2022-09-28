9fb16929def7de3a80e316a619cd4d65f630e52d
PawelPalasinski <pawel.palasinski@gmail.com>
2022-09-29 01:07:15 +0200

    footer

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -10,7 +10,7 @@ import friendList from '../data/friends.json';
 import { TransactionHistory } from './TransactionHistory/TransactionHistory';
 import transactions from '../data/transactions.json';
 
-import { Footer } from './Footer/Footer'
+import { Footer } from './Footer/Footer';
 
 export const App = () => {
   const { username, tag, location, avatar, stats } = user;
@@ -39,7 +39,7 @@ export const App = () => {
         <TransactionHistory items={transactions} />
       </>
 
-      <Footer/>
+      <Footer />
     </>
   );
 };
--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,8 +1,7 @@
 import styles from './Footer.module.css';
 
-
 export const Footer = () => (
-    <footer className={styles.footer}>
+  <footer className={styles.footer}>
     <p>Paweł Pałasiński © 2022</p>
   </footer>
 );
